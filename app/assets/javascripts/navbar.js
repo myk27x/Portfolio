159798bcf2c5a1f1a5e23a033ea2129d83f26c4a
Michael Reed <myk27x@yahoo.com>
2016-01-18 12:22:53 -0500

    moved relevant navbar js here
    adds jquery page offset animation

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('#toTop').click(function(){
+    console.log("I clicked");
+    $('body').animate({scrollTop: $('#header').offset().top}, 600);
+  })
+
+  $('#toAbout').click(function(){
+    $('body').animate({scrollTop: $('#about').offset().top - 10}, 600);
+  });
+
+  $('#toProjects').click(function(){
+    $('body').animate({scrollTop: $('#projects').offset().top -15}, 600);
+  });
+
+  $('#contact').children('.navlinks').click(function(){
+    $('#info').slideToggle(500);
+    if ($(this).css('display') == 'block') {
+      $('#info').css('display', 'flex')
+    }
+  });
+
+});
