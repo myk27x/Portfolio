bf321e3b77fbed9149c54196671b3134515031fd
Michael Reed <myk27x@yahoo.com>
2016-01-18 01:17:41 -0500

    adds projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118004157) do
+ActiveRecord::Schema.define(version: 20160118061135) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
