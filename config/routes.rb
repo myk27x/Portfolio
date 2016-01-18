74953186a90d84377d1018c42ac99636ef460389
Michael Reed <myk27x@yahoo.com>
2016-01-18 00:56:08 -0500

    adds basic routing for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  root to: 'portfolio#index'
+  root to:         'portfolio#index'
 
-  get 'email' => 'email#new'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get  'email'  => 'email#new'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get  'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  get  'logout' => 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
