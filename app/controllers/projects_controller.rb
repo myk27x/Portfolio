05f95e4b661b008197f9a2e59f210dd45ad28eea
Michael Reed <myk27x@yahoo.com>
2016-01-20 01:48:53 -0500

    adds methods new, create, project_params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,18 @@
 class ProjectsController < ApplicationController
-  before_action :authorize
+  before_action :authorize, except: [:show]
+
+  def new
+    @projects = Project.all
+  end
+
+  def create
+    Project.create(project_params)
+    render new_project_path
+  end
+
+private
+  def project_params
+    params.require(:project).permit(:title, :image)
+  end
+
 end
