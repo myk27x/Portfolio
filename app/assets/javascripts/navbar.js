266c1bb6202e1bdd80ae91334daaa44e7584fd36
Michael Reed <myk27x@yahoo.com>
2016-02-07 12:27:33 -0500

    DRY-ing up code

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
 
-  $('#toTop').click(function(){
-    $('body').animate({scrollTop: $('#header').offset().top}, 600);
-  })
+  function view(clicked, goto) {
+    $(clicked).click(function(){
+      $('body').animate({scrollTop: $(goto).offset().top}, 600);
+    })
+  }
 
-  $('#toAbout').click(function(){
-    $('body').animate({scrollTop: $('#about').offset().top - 35}, 600);
-  });
-
-  $('#toProjects').click(function(){
-    $('body').animate({scrollTop: $('#projects').offset().top -32}, 600);
-  });
-
-  $('#toContact').click(function(){
-    $('body').animate({scrollTop: $('#contact').offset().top}, 600);
-  });
+  view('#toTop'     , '#header');
+  view('#toAbout'   , '#about');
+  view('#toProjects', '#projects');
+  view('#toContact' , '#contact');
 
 });
