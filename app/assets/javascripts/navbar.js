71d599eaacf678857e67dc3f38773cb16bf69a0d
Michael Reed <myk27x@yahoo.com>
2016-02-09 18:10:53 -0500

    removed test code and minor changes

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   function view(clicked, goto) {
     $(clicked).click(function(){
       $('body').animate({scrollTop: $(goto).offset().top}, 1300);
-      // $(this).children().fadeIn();
     })
   }
 
