aec8be3164b6636306c504003b1b1f581a4e2579
Michael Reed <myk27x@yahoo.com>
2016-02-13 22:25:46 -0500

    temporary hold on badges but may revisit

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,8 +2,7 @@ class PortfolioController < ApplicationController
 
   def index
     @project = Project.all
-    @badges = ["Ruby-edit.jpg", "Rails-edit.png", "HTML5-edit.jpg", "CSS3-edit.jpg", "JS-edit.jpg",
-               "AJAX.svg.png", "jquery-logo.png", "json-logo.png", "haml-edit.png", "github.png", "heroku.png", "namecheap.png", "mailgun.png", "twilio.png", "sqlite3.png", "postgres.png", "bootstrap.png", "tiy.png", "codecademy-short.jpg", "exercism.png"]
+    # @badges = ["Ruby-edit.jpg", "Rails-edit.png", "HTML5-edit.jpg", "CSS3-edit.jpg", "JS-edit.jpg", "AJAX.svg.png", "jquery-logo.png", "json-logo.png", "haml-edit.png", "github.png", "heroku.png", "namecheap.png", "mailgun.png", "twilio.png", "sqlite3.png", "postgres.png", "bootstrap.png", "tiy.png", "codecademy-short.jpg", "exercism.png"]
   end
 
 end
